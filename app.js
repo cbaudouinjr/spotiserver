06c66c17463802c643d5794d9131a8e6db7c8789
Chris Baudouin, Jr <cjb5326@rit.edu>
2018-12-14 21:42:05 -0500

    - Removes public, routes and views directories - Adds config.json - Starts base development for app.js

--- app.js
+++ app.js
@@ -1,41 +1,23 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var SpotifyWebAPI = require('spotify-web-api-node');
+var config = require("./config");
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+var spotify_client = new SpotifyWebAPI({
+  clientid : config.clientID,
+  clientSecret  : config.clientSecret,
+  redirectUri : config.redirectURI
+    }
+);
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.post('/request', function (req, res) {
+  var track_name = req.query.trackName;
+  var artist = req.query.artist;
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
